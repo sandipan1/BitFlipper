89b22929e6a68897fd9c28472ba67c9c708651f1
Joy Chopra <joy.chopra2@gmail.com>
2018-02-07 11:45:31 +0530

    Update __init__.py

--- gym_BitFlipper/__init__.py
+++ gym_BitFlipper/__init__.py
@@ -1,7 +1,13 @@
 from gym.envs.registration import register
-
+#id='BitFlipper-n:space_seed'
 register(
-    id='BitFlipper-v0',
+    id='BitFlipper-10:0',
     entry_point='BitFlipper.gym_BitFlipper.envs:BitFlipperEnv',
     kwargs = {"space_seed":0,"n":10}
+)
+
+register(
+    id='BitFlipper-15:0',
+    entry_point='BitFlipper.gym_BitFlipper.envs:BitFlipperEnv',
+    kwargs = {"space_seed":0,"n":15}
 )
