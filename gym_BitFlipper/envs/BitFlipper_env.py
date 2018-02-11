d836f66c43e20164c3b845c75dbb761b36c32e43
Sandipan Haldar <h.sandipan05@gmail.com>
2018-02-11 20:10:00 +0530

    Update BitFlipper_env.py

--- gym_BitFlipper/envs/BitFlipper_env.py
+++ gym_BitFlipper/envs/BitFlipper_env.py
@@ -56,6 +56,7 @@ class BitFlipperEnv(gym.Env):
   def seed(self, seed=None):
     #self.np_random, seed = seeding.np_random(seed)
     #return [seed]
+    pass
   
   def bitflip(self,index):
     s2=np.array(self.state)
