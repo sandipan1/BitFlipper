9b6191924da0b80c5b70e968ad643aee20840f89
Sandipan Haldar <h.sandipan05@gmail.com>
2018-02-15 22:19:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_BitFlipper',
       version='0.0.1',
-      install_requires=['gym>=0.2.3','baselines'] 
+      install_requires=['gym>=0.2.3','baselines','dqn_her']
+      dependency_links=['http://github.com/sandipan1/dqn_her/master#egg=package-1.0']
 )  
