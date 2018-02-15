26ea12d3f2173d43be4e21dff0318acad34703cc
Sandipan Haldar <h.sandipan05@gmail.com>
2018-02-15 23:35:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup
 
 setup(name='gym_BitFlipper',
       version='0.0.1',
-      install_requires=['gym>=0.2.3','baselines','dqn_her']
-      dependency_links=['https://github.com/sandipan1/dqn_her/tarball/master#egg=package-1.0'],
+      install_requires=['gym>=0.2.3','baselines','dqn_her'],
 )  
